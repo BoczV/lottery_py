35c86c562294f7b5121957e9b7fd80d88050807c
Bócz Viktor <56642389+BoczV@users.noreply.github.com>
2023-03-18 10:15:21 +0100

    Beta version of main.py is finished

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
+from user_guesses import make_guess
+from lottery import pick_five_random_numbers
+
 def main():
     print("Hey there and welcome! Lottery will start!")
+    chosen_numbers = make_guess()
+    print(f"You choose these numbers: {chosen_numbers}")
+    lottery_numbers = pick_five_random_numbers()
+    print(f"Lottery numbers: {lottery_numbers}")
+    count_matches(chosen_numbers, lottery_numbers)
 
-  
+
+def count_matches(chosen_numbers, lottery_numbers):
+    counter = 0
+    for element in chosen_numbers:
+        if(element in lottery_numbers):
+            counter += 1
+    print(f"You have {counter} matches.")
+    if(counter > 3):
+        print(f"Hurray! You won!")
+    else:
+        print("Better luck next time!")
+
+
 if __name__=="__main__":
     main()
